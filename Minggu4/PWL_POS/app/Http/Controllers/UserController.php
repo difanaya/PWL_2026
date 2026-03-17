a8d7ea90147d43293960ee574170aac9c8628697
Difa Naya <difapolinema@gmail.com>
2026-03-17 15:25:15 +0700

    praktikum 2.1

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,17 +10,17 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-            // 'level_id' => 2,
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            // 'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        //     // 'level_id' => 2,
+        //     // 'username' => 'manager_dua',
+        //     // 'nama' => 'Manager 2',
+        //     // 'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
         // $data = [
         //     'username' => 'customer-1',
         //     'nama' => 'Pelanggan',
@@ -34,7 +34,12 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        //$user = UserModel::find(1);
+        //$user = UserModel::where('level_id', 1)->first();
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
